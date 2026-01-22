8859ba1cd7d3bdc1f91722ad26ec43eb549530c8
AlinaK <yeetaut@gmail.com>
2026-01-22 10:56:19 +0500

    Added non polite greeting

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 name1 = 'Misha'
-#name2 = 'Toma'
+name2 = 'Toma'
 print('Hello,', name1, ', nice to see you!')
 
-#print('Hello,', name2, "it's not that nice to see you..")
+print('Hello,', name2, "it's not that nice to see you..")
