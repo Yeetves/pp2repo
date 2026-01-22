46c4f2d0f2ebabe9f663e85dbb8b53f67ffede01
AlinaK <yeetaut@gmail.com>
2026-01-22 11:56:25 +0500

    Variables, names and assigning multiple values

--- byebye.py
+++ byebye.py
@@ -1,1 +1,9 @@
-print("Byebye...")
+someWaysToCallMisha = ["Mikhail", "Misha", "Mish", "Mishunya"]
+name1, name2, name3, name4 = someWaysToCallMisha
+
+name3 = name2 #they are kinda the same
+
+print("Byebye...", name4)
+print("Bye...", name3)
+print("See you later,", name2)
+print("Good bye,", name1)
