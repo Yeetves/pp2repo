1bc59c190e189cfecee50cd98998c400669ae355
AlinaK <yeetaut@gmail.com>
2026-01-22 11:31:45 +0500

    Testing conditions

---
+++ syntax.py
@@ -0,0 +1,11 @@
+great = 60
+cooked = 29
+
+score = int(input("What's your score for both attestations?"))
+
+if score > cooked and score <= great:
+    print("Great! u've still got hope")
+#elif score > great or score < 0:
+    #print("liar.")
+else:
+    print("you are cooked!")
