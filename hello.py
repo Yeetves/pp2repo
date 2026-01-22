469d379b82d58a720663e0d3a128adc89e742317
AlinaK <yeetaut@gmail.com>
2026-01-22 11:40:00 +0500

    Fixed spaces in print() function

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 name1 = 'Misha'
 name2 = 'Toma'
-print('Hello,', name1, ', nice to see you!')
+print('Hello, ', name1, '. Nice to see you!', sep="")
 
-print('Hello,', name2, ". It's not that nice to see you..")
+print('Hello, ', name2, ". It's not that nice to see you..", sep="")
